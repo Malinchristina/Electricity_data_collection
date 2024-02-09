e8393934dc90c35e14c99fd4a88ff7d639ae0dda
Malin Nilsson <malinnilsson@Malins-Air.lan>
2024-02-09 17:27:10 +0100

    Add creds and try connecting to google sheets

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("pp3_el")
+SHEET = GSPREAD_CLIENT.open("pp3-el")
 
 costs = SHEET.worksheet("costs")
 
