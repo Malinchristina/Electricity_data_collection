169ef52470f2ee909b420b9f1171c3f328f589a2
Malinchristina <malin_christina@yahoo.se>
2024-02-11 16:49:30 +0100

    Add function to ask for electricity fee

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ def get_monthly_fee():
     Ask user to enter monthly fee data
     """
     print("Please enter monthly fee data from electricity company.")
-    print ("Data should be a two digit number. Example 20.\n")
+    print("Data should be a two digit number. Example 20.\n")
 
     # Check so exactly 2 digit number is entered.
     while True:
@@ -26,10 +26,31 @@ def get_monthly_fee():
             return int(data_str)
         else:
             print("Invalid input, please enter a 2 digit number.\n")
-            
-        
-# def validate_monthly_fee(value):
-  #  print(value)
-# testing
 
-get_monthly_fee()
+            
+      
+def get_electricity_fee():
+    """
+    Ask user to enter electricity fee data
+    """
+    print("Please enter electricity fee data from electricity company.")
+    print ("Data should be a number with 1 decimal. Example 1.5.\n")
+
+    # Check if float number with 1 decimal is entered.
+    while True:
+        data_str = input("Enter your data here:")
+        try:
+            data = float(data_str)
+            if data.is_integer():
+                print("Invalid input, please enter a number with one decimal place\n")
+            elif round(data, 1) == data: #check if number has one decimal
+                return data 
+            else:
+                print("Invalid input, please enter a number with one decimal place\n")
+        except ValueError:
+            print("Invalid input, please enter a number with one decimal place\n")
+
+     
+
+monthly_fee = get_monthly_fee()
+electricity_fee = get_electricity_fee()
